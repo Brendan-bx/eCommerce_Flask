98ad192b68465d75e8cddb38837221f4e50d48bb
Makkho <mamafort687@gmail.com>
2023-12-12 19:52:09 +0100

    New databse

--- projet/market.py
+++ projet/market.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
 db=SQLAlchemy(app)
+
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(),primary_key = True)
@@ -28,4 +31,5 @@ def market_page():
 
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
