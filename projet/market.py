902f0c793cb2dc9508d4d1ac46572a424b287d1e
Brendan Bx <Brendanbx@gmail.com>
2023-12-13 15:05:08 +0100

    Inscription et Connexion features

--- projet/market.py
+++ projet/market.py
@@ -1,36 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
-db=SQLAlchemy(app)
-
-
-
-class Item(db.Model):
-    id = db.Column(db.Integer(),primary_key = True)
-    name=db.Column(db.String(length=50),nullable=False, unique=True)
-    barcode=db.Column(db.String(length=12),nullable=False, unique=True)
-    price=db.Column(db.Integer(), nullable=False)
-    description=db.Column(db.String(length=1024),nullable=False, unique=True)
-
-
-@app.route("/")
-
-@app.route("/home")
-def home_page():
-    return render_template('home.html')
-
-@app.route("/market")
-def market_page():
-    return render_template('market.html', item_name = [{'id' :1, 'name':'Mobile', 'barcode':'12345679012', 'price':'500'},
-                                                     {'id' :2, 'name':'Laptop', 'barcode':'12345679013', 'price':'1000'},
-                                                     {'id' :3, 'name':'Earphone', 'barcode':'12345679014', 'price':'200'}])
-
+from market import app
 
 
 if __name__ == '__main__':
-     with app.app_context():
-        db.create_all()
      app.run(debug=True)
---
+++ projet/market/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, Email, EqualTo, DataRequired, ValidationError
+from market.model import User
+
+class RegisterForm(FlaskForm):
+
+  def validate_username(self, username_to_check):
+    user = User.query.filter_by(username=username_to_check.data).first()
+    if user:
+      raise ValidationError("Username already exists. Please try a different username.")
+  
+  def validate_email(self, email_to_check):
+    email = User.query.filter_by(email_address=email_to_check.data).first()
+    if email:
+      raise ValidationError("Email address already registered in the site.")
+
+  username = StringField(label="Username: ", validators=[Length(min=5,max=30), DataRequired()])
+  email = StringField(label="Email Address: ", validators=[Email(), DataRequired()])
+  password1 = PasswordField(label= "Password: ", validators=[Length(min=5), DataRequired()])
+  password2 = PasswordField(label="Confirm Password: ", validators=[EqualTo('password1'), DataRequired()])
+  submit = SubmitField(label= "Register")
+
+class LoginForm(FlaskForm):
+    username = StringField(label="Username: ", validators=[DataRequired()])
+    password = PasswordField(label= "Password: ", validators=[DataRequired()])
+    submit = SubmitField(label="Login")
