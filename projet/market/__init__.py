00e9615e378c9078ccc116d23bea25699702391c
Makkho <mamafort687@gmail.com>
2023-12-13 16:27:12 +0100

    Ajout du lougout, mise en place du info

--- projet/market/__init__.py
+++ projet/market/__init__.py
@@ -4,9 +4,11 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///market.db'
-app.config['SECRET_KEY']='2082bef184a3495dff2473bf'
-db=SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "2082bef184a3495dff2473bf"
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
--- projet/market/routes.py
+++ projet/market/routes.py
@@ -3,42 +3,65 @@ from flask import render_template, redirect, url_for, get_flashed_messages, flas
 from market.model import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
+
 
 @app.route("/")
-
 @app.route("/home")
 def home_page():
-    return render_template('home.html')
+    return render_template("home.html")
+
 
 @app.route("/market")
+@login_required
 def market_page():
-    return render_template('market.html', item_name=Item.query.all())
+    return render_template("market.html", item_name=Item.query.all())
 
-@app.route("/register", methods=['GET','POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
-    form=RegisterForm()
+    form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,
-                              email_address = form.email.data,
-                              password = form.password1.data)
+        user_to_create = User(
+            username=form.username.data,
+            email_address=form.email.data,
+            password=form.password1.data,
+        )
         db.session.add(user_to_create)
         db.session.commit()
-        return redirect(url_for('market_page'))
+        return redirect(url_for("market_page"))
     if form.errors != {}:
         for err_msg in form.errors.values():
-            flash(f'There is an error while creating this user. The error is {err_msg}', category='danger')
-    return render_template('register.html', form=form)
+            flash(
+                f"There is an error while creating this user. The error is {err_msg}",
+                category="danger",
+            )
+    return render_template("register.html", form=form)
 
-@app.route("/login", methods=['GET','POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login_page():
     form = LoginForm()
     if form.validate_on_submit():
         attempted_user = User.query.filter_by(username=form.username.data).first()
-        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
             login_user(attempted_user)
-            flash(f"Welcome {attempted_user.username}. You have successfully logged in", category="success")
-            return redirect(url_for('market_page'))
+            flash(
+                f"Welcome {attempted_user.username}. You have successfully logged in",
+                category="success",
+            )
+            return redirect(url_for("market_page"))
         else:
-            flash("Username and Password not matched. Please try again", category="danger")
-    return render_template('login.html', form=form)
+            flash(
+                "Username and Password not matched. Please try again", category="danger"
+            )
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+def logout_page():
+    logout_user()
+    flash("Vous avez été déconnecté avec succées", category="info")
+    return redirect(url_for("home_page"))
