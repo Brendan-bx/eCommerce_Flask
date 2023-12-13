05327201a2f81c0d20842ea69f096595b2e8abea
Makkho <mamafort687@gmail.com>
2023-12-13 23:12:52 +0100

    Code toute écrit mais marche pas
    problème avec les boutons,  qui ne veux pas s'afficher, et qui  marche pas, avec le {{item.id}}

--- projet/market/routes.py
+++ projet/market/routes.py
@@ -1,9 +1,16 @@
 from market import app
-from flask import render_template, redirect, url_for, get_flashed_messages, flash
+from flask import (
+    render_template,
+    redirect,
+    url_for,
+    get_flashed_messages,
+    flash,
+    request,
+)
 from market.model import Item, User
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm, purchaseform, sellform
 from market import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route("/")
@@ -12,10 +19,47 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/market")
+@app.route("/market", methods=["GET", "POST"])
 @login_required
 def market_page():
-    return render_template("market.html", item_name=Item.query.all())
+    purchase_form = purchaseform()
+    sell_form = sellform()
+    if request.method == "POST":
+        purchased_item = request.form.get("purchase_item")
+        P_item_obj = Item.query.filter_by(name=purchased_item).first()
+        if P_item_obj:
+            if current_user.can_buy(P_item_obj):
+                P_item_obj.buy()
+                flash(
+                    f"You have succesfully bought {P_item_obj.name} for {P_item_obj.price}",
+                    category="success",
+                )
+                return redirect(url_for("market_page"))
+            else:
+                flash(f"Sorry You have insufficient fund to buy")
+        sell_item = request.form.get("sell_item")
+        s_item_obj = Item.query.filter_by(name=sell_item).first()
+        if s_item_obj:
+            if current_user.can_sell(current_user):
+                s_item_obj.sell(current_user)
+
+        return redirect(url_for("market_page"))
+
+    if request.method == "GET":
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template(
+            "market.html",
+            item_name=Item.query.filter_by(owner=""),
+            purchase_form=purchase_form,
+            owned_items=owned_items,
+            sell_form=sell_form,
+        )
+
+    return render_template(
+        "market.html",
+        item_name=Item.query.filter_by(owner=""),
+        purchase_form=purchase_form,
+    )
 
 
 @app.route("/register", methods=["GET", "POST"])
